1bd589b8e7b9c527df92e175b3db2fc8c098bd12
Jeremy Bourgne <jeremy.bourgne.louis@gmail.com>
2020-06-26 17:27:20 +0200

    Add error handling and MVC routing

--- AidImpactTraining/Startup.cs
+++ AidImpactTraining/Startup.cs
@@ -5,26 +5,62 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
 
 namespace AidImpactTraining
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration configuration;
+
+        // this constructor populate the configuration automatically when it starts
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>(feature => new FeatureToggles
+            {
+                DeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
+
+            // I disabled the endpointRouting principally because I learned with the MVC methods for the routing. So that's why
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IWebHostEnvironment env,
+            FeatureToggles features)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
-            }
+            } 
+
+            // Test the exception with a specific route (if it contains "invalid" for example)
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new Exception("Erreur, ce chemin est invalide. Veuillez nous excuser");
+                }
+                await next();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default",
+                    "{controller=Home}/{action=Index}/{id?}");
+            });
 
             // Allows me to show my server files in the "wwwroot" folder
             app.UseFileServer();
